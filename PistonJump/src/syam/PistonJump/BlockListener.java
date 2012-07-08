8f8dd4f952f2753ba49e08b40366f7953ecf0dbf
syamn <admin@sakura-server.net>
2012-07-08 23:50:07 +0900

    Add GPLv3 License provision

--- PistonJump/src/syam/PistonJump/BlockListener.java
+++ PistonJump/src/syam/PistonJump/BlockListener.java
@@ -2,28 +2,33 @@ package syam.PistonJump;
 
 import java.util.logging.Logger;
 
-import net.minecraft.server.EntityFallingBlock;
-import net.minecraft.server.Packet51MapChunk;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.craftbukkit.CraftWorld;
-import org.bukkit.craftbukkit.entity.CraftPlayer;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.FallingSand;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPistonExtendEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
+
+/*     Copyright (C) 2012  syamn <admin@sakura-server.net>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 public class BlockListener implements Listener {
 	public final static Logger log = PistonJump.log;
